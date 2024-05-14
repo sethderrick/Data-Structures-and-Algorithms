5ba815d66002a552da0a6a097807572ad33622a2
Seth Derrick <seth@sethderrick.tech>
2024-05-14 18:23:27 -0500

    Fix compile errors in linkedList

--- 03_linkedLists/linkedListImplementation.rs
+++ 03_linkedLists/linkedListImplementation.rs
@@ -1,144 +1,139 @@
-/*
-  Implementation uses Rust generics to handle data of any type.
+use std::fmt::Debug;
 
-  Implemented using Rust's ownership model, ensuring that each
-  element in the list is managed correctly to prevent memory leaks
-  and dangling references.
-
-  Option and Box are used for managing nodes, where Option handles
-  the possibility of null values, and Box provides a way to allocate
-  nodes on the heap.
-
-  Inserting and removing nodes involves manipulating pointers to ensure
-  that the list maintains correct links between nodes.
-*/
-
-/// A node in the singly linked list, containing a value and an optional next node.
+#[derive(Clone)]
 struct Node<T> {
     value: T,
     next: Option<Box<Node<T>>>,
 }
 
-/// A singly linked list with basic operations such as append, prepend, insert, and remove.
 struct LinkedList<T> {
     head: Option<Box<Node<T>>>,
-    tail: Option<Box<Node<T>>>,
+    tail: Option<*mut Node<T>>, // Using raw pointer for mutability
     length: usize,
 }
 
-impl<T> LinkedList<T>
-where
-    T: Clone,
-{
-    /// Creates a new singly linked list with an initial value.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - The initial value to be placed in the list.
-    pub fn new(value: T) -> Self {
-        let node = Box::new(Node { value, next: None });
+impl<T: Clone + Debug> LinkedList<T> {
+    fn new(value: T) -> Self {
+        let new_node = Box::new(Node { value, next: None });
+        let raw_node: *mut _ = Box::into_raw(new_node.clone());
+
         LinkedList {
-            head: Some(node.clone()),
-            tail: Some(node),
+            head: Some(new_node),
+            tail: Some(raw_node),
             length: 1,
         }
     }
 
-    /// Appends a value to the end of the list.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - The value to append to the list.
-    pub fn append(&mut self, value: T) {
-        let new_node = Box::new(Node { value, next: None });
-        if let Some(ref mut tail) = self.tail {
-            tail.next = Some(new_node);
+    fn append(&mut self, value: T) {
+        let mut new_node = Box::new(Node { value, next: None });
+        let raw_node: *mut _ = &mut *new_node;
+
+        if let Some(tail) = self.tail {
+            unsafe {
+                (*tail).next = Some(new_node);
+            }
+        } else {
+            self.head = Some(new_node.clone());
         }
-        self.tail = self
-            .head
-            .as_mut()
-            .and_then(|node| node.next.as_mut())
-            .map(|node| node as &mut Box<Node<T>>);
+
+        self.tail = Some(raw_node);
         self.length += 1;
     }
 
-    /// Prepends a value to the beginning of the list.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - The value to prepend.
-    pub fn prepend(&mut self, value: T) {
-        let new_node = Box::new(Node {
+    fn prepend(&mut self, value: T) {
+        let mut new_node = Box::new(Node {
             value,
             next: self.head.take(),
         });
+
+        let raw_node: *mut _ = &mut *new_node;
         self.head = Some(new_node);
-        if self.length == 0 {
-            self.tail = self.head.as_mut().map(|node| node as &mut Box<Node<T>>);
+
+        if self.tail.is_none() {
+            self.tail = Some(raw_node);
         }
+
         self.length += 1;
     }
 
-    /// Inserts a value at a specified index in the list.
-    ///
-    /// # Arguments
-    ///
-    /// * `index` - The zero-based index where the value should be inserted.
-    /// * `value` - The value to insert.
-    pub fn insert(&mut self, index: usize, value: T) {
-        if index == 0 {
-            self.prepend(value);
-            return;
-        }
+    fn insert(&mut self, index: usize, value: T) {
         if index >= self.length {
-            self.append(value);
-            return;
+            return self.append(value);
         }
 
-        let mut current = &mut self.head;
-        for _ in 0..index - 1 {
-            current = &mut current.as_mut().unwrap().next;
-        }
-        let new_node = Box::new(Node {
-            value,
-            next: current.as_mut().unwrap().next.take(),
-        });
-        current.as_mut().unwrap().next = Some(new_node);
-        self.length += 1;
-    }
-
-    /// Removes a node from the list at the specified index.
-    ///
-    /// # Arguments
-    ///
-    /// * `index` - The zero-based index of the node to remove.
-    pub fn remove(&mut self, index: usize) {
         if index == 0 {
-            self.head = self.head.take().and_then(|node| node.next);
-            self.length -= 1;
-            return;
+            return self.prepend(value);
         }
-        let mut current = &mut self.head;
+
+        let mut current = self.head.as_mut();
         for _ in 0..index - 1 {
-            current = &mut current.as_mut().unwrap().next;
+            if let Some(current_node) = current {
+                current = current_node.next.as_mut();
+            } else {
+                return;
+            }
         }
-        let next_node = current.as_mut().unwrap().next.take().unwrap().next;
-        current.as_mut().unwrap().next = next_node;
-        if index == self.length - 1 {
-            self.tail = Some(current.as_mut().unwrap().clone());
+
+        if let Some(current_node) = current {
+            let mut new_node = Box::new(Node {
+                value,
+                next: current_node.next.take(),
+            });
+            current_node.next = Some(new_node);
+            self.length += 1;
         }
-        self.length -= 1;
     }
 
-    /// Converts the list into a vector of its elements.
-    pub fn to_vec(&self) -> Vec<T> {
-        let mut vec = Vec::new();
+    fn remove(&mut self, index: usize) {
+        if index >= self.length {
+            return;
+        }
+
+        if index == 0 {
+            if let Some(mut head_node) = self.head.take() {
+                self.head = head_node.next.take();
+
+                if self.length == 1 {
+                    self.tail = None;
+                }
+                self.length -= 1;
+                return;
+            }
+        }
+
+        let mut current = self.head.as_mut();
+        for _ in 0..index - 1 {
+            if let Some(current_node) = current {
+                current = current_node.next.as_mut();
+            } else {
+                return;
+            }
+        }
+
+        if let Some(current_node) = current {
+            let mut node_to_remove = current_node.next.take();
+            if let Some(ref mut next_node) = node_to_remove {
+                current_node.next = next_node.next.take();
+
+                if index == self.length - 1 {
+                    self.tail = Some(Box::into_raw(next_node.clone()));
+                }
+            }
+
+            self.length -= 1;
+        }
+    }
+
+    fn to_vec(&self) -> Vec<T> {
+        let mut result = Vec::new();
         let mut current = &self.head;
+
         while let Some(ref node) = current {
-            vec.push(node.value.clone());
+            result.push(node.value.clone());
             current = &node.next;
         }
-        vec
+
+        result
     }
 }
 

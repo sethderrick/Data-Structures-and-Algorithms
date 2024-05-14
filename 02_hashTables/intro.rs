783b8f9b5de6522b5ceb6466b6cf1cafa1a5b379
Seth Derrick <seth@sethderrick.tech>
2024-05-14 18:45:33 -0500

    Fix compile errors for hashTable examples

--- 02_hashTables/intro.rs
+++ 02_hashTables/intro.rs
@@ -1,10 +1,21 @@
+#[derive(Debug)]
 struct User {
     name: String,
     age: u32,
     magic: bool,
+    spell: String,
 }
 
 impl User {
+    fn new(name: String, age: u32, magic: bool) -> Self {
+        User {
+            name,
+            age,
+            magic,
+            spell: String::from(""), // Initialize the spell field
+        }
+    }
+
     fn scream(&self) {
         println!("Gaaaaaaggh!");
     }
@@ -15,9 +26,11 @@ fn main() {
         name: String::from("Seth"),
         age: 54,
         magic: true,
+        spell: String::from(""),
     };
 
     println!("{}", user.name); // Lookup O(1)
     user.spell = String::from("abra kadabra"); // Insert O(1)
-    println!("{:?}", user);
+                                               // println!("{:?}", user);
+    println!("{:?}", user); // Lookup O(1)
 }

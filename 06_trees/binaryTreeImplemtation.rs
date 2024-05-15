063cf9e2d42b6fd7bba4177a29294ae86d29d9a8
Seth Derrick <seth@sethderrick.tech>
2024-05-15 09:16:11 -0500

    Make 06 trees compile (this was a pain in the ass!)

--- 06_trees/binaryTreeImplemtation.rs
+++ 06_trees/binaryTreeImplemtation.rs
@@ -1,152 +1,101 @@
-/// A node in the binary search tree, containing a value and pointers to the left and right child nodes.
+use std::cmp::Ordering;
+
+/// A Node struct that stores a value and links to left and right children in a binary search tree.
+#[derive(Debug)]
 struct Node<T> {
     value: T,
     left: Option<Box<Node<T>>>,
     right: Option<Box<Node<T>>>,
 }
 
-/// A Binary Search Tree (BST) with operations for inserting, looking up, and removing elements.
+impl<T: Ord> Node<T> {
+    fn new(value: T) -> Self {
+        Node {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+/// A BinarySearchTree struct that uses a linked list of Nodes to store elements in a binary search tree.
+#[derive(Debug)]
 struct BinarySearchTree<T> {
     root: Option<Box<Node<T>>>,
 }
 
-impl<T: PartialOrd + Copy> BinarySearchTree<T> {
+impl<T: Ord> BinarySearchTree<T> {
     /// Constructs a new, empty BinarySearchTree.
     pub fn new() -> Self {
         BinarySearchTree { root: None }
     }
 
     /// Inserts a new value into the binary search tree.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - The value to be inserted into the tree.
     pub fn insert(&mut self, value: T) {
-        let new_node = Box::new(Node {
-            value,
-            left: None,
-            right: None,
-        });
-
-        if self.root.is_none() {
-            self.root = Some(new_node);
-        } else {
-            let mut current = self.root.as_mut().unwrap();
-            loop {
-                if value < current.value {
-                    if current.left.is_none() {
-                        current.left = Some(new_node);
-                        break;
-                    } else {
-                        current = current.left.as_mut().unwrap();
-                    }
-                } else {
-                    if current.right.is_none() {
-                        current.right = Some(new_node);
-                        break;
-                    } else {
-                        current = current.right.as_mut().unwrap();
-                    }
-                }
+        let mut current = &mut self.root;
+        while let Some(ref mut found) = current {
+            match value.cmp(&found.value) {
+                Ordering::Less => current = &mut found.left,
+                Ordering::Greater => current = &mut found.right,
+                Ordering::Equal => return, // Ignore duplicates
             }
         }
+        *current = Some(Box::new(Node::new(value)));
     }
 
-    /// Looks up a value in the binary search tree and returns a reference to the node containing it if found.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - The value to look for in the tree.
+    /// Finds a value in the binary search tree and returns a reference to it, if it exists.
     pub fn lookup(&self, value: T) -> Option<&Node<T>> {
-        let mut current = self.root.as_ref();
-        while let Some(node) = current {
-            if value < node.value {
-                current = node.left.as_ref();
-            } else if value > node.value {
-                current = node.right.as_ref();
-            } else {
-                return Some(node);
+        let mut current = &self.root;
+        while let Some(ref found) = current {
+            match value.cmp(&found.value) {
+                Ordering::Less => current = &found.left,
+                Ordering::Greater => current = &found.right,
+                Ordering::Equal => return Some(found),
             }
         }
         None
     }
 
-    /// Removes a value from the binary search tree.
-    ///
-    /// # Arguments
-    ///
-    /// * `value` - The value to remove from the tree.
-    pub fn remove(&mut self, value: T) -> bool {
-        let mut current = self.root.as_mut();
-        let mut parent = None;
-
-        while let Some(ref mut found) = current {
-            if value < found.value {
-                parent = current;
-                current = found.left.as_mut();
-            } else if value > found.value {
-                parent = current;
-                current = found.right.as_mut();
-            } else {
-                // Element found: handle the removal.
-                if found.right.is_none() {
-                    let new_link = found.left.take();
-                    if let Some(parent_node) = parent {
-                        if parent_node.value > value {
-                            parent_node.left = new_link;
-                        } else {
-                            parent_node.right = new_link;
-                        }
-                    } else {
-                        self.root = new_link;
-                    }
-                } else if found.right.as_ref().unwrap().left.is_none() {
-                    let right_child = found.right.take().unwrap();
-                    right_child.left = found.left.take();
-                    if let Some(parent_node) = parent {
-                        if parent_node.value > value {
-                            parent_node.left = Some(right_child);
-                        } else {
-                            parent_node.right = Some(right_child);
-                        }
-                    } else {
-                        self.root = Some(right_child);
-                    }
-                } else {
-                    // Find the smallest value in the right subtree.
-                    let (smallest, smallest_parent) =
-                        Self::find_min(&mut found.right.as_mut().unwrap());
-                    smallest.left = found.left.take();
-                    smallest.right = found.right.take();
-                    if let Some(parent_node) = smallest_parent {
-                        parent_node.left = smallest.right.take();
-                    }
-                    if let Some(parent_node) = parent {
-                        if parent_node.value > value {
-                            parent_node.left = Some(smallest);
-                        } else {
-                            parent_node.right = Some(smallest);
-                        }
-                    } else {
-                        self.root = Some(smallest);
-                    }
-                }
-                return true;
-            }
-        }
-        false
+    /// Deletes a value from the binary search tree, if it exists.
+    pub fn remove(&mut self, value: T) {
+        self.root = BinarySearchTree::remove_node(self.root.take(), value);
     }
 
-    /// Helper function to find the minimum node and its parent in a subtree.
-    fn find_min(node: &mut Box<Node<T>>) -> (Box<Node<T>>, Option<&mut Box<Node<T>>>) {
-        let mut current = node;
-        let mut parent = None;
-
-        while let Some(ref mut next) = current.left {
-            parent = Some(current);
-            current = next;
+    fn remove_node(node: Option<Box<Node<T>>>, value: T) -> Option<Box<Node<T>>> {
+        let mut node = node?;
+        match value.cmp(&node.value) {
+            Ordering::Less => {
+                node.left = BinarySearchTree::remove_node(node.left.take(), value);
+                Some(node)
+            }
+            Ordering::Greater => {
+                node.right = BinarySearchTree::remove_node(node.right.take(), value);
+                Some(node)
+            }
+            Ordering::Equal => match (node.left.take(), node.right.take()) {
+                (None, None) => None,
+                (Some(left), None) => Some(left),
+                (None, Some(right)) => Some(right),
+                (Some(left), Some(right)) => {
+                    let (min_right, right) = BinarySearchTree::extract_min(right);
+                    node.value = min_right.value;
+                    node.right = right;
+                    node.left = Some(left);
+                    Some(node)
+                }
+            },
         }
-        (current.left.take().unwrap(), parent)
+    }
+
+    fn extract_min(mut node: Box<Node<T>>) -> (Box<Node<T>>, Option<Box<Node<T>>>) {
+        if let Some(left) = node.left.take() {
+            let (min_left, left) = BinarySearchTree::extract_min(left);
+            node.left = left;
+            (min_left, Some(node))
+        } else {
+            let right = node.right.take();
+            (node, right)
+        }
     }
 }
 
